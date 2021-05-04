addd407f4ed1b76c81e1d0ddca258e64285eeab4
Nick Rose <nihk@github.com>
2021-05-03 21:53:14 -0400

    Simplify scanner delay config

--- app/src/main/kotlin/nick/template/data/BluetoothScanner.kt
+++ app/src/main/kotlin/nick/template/data/BluetoothScanner.kt
@@ -1,14 +1,12 @@
 package nick.template.data
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.conflate
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 interface BluetoothScanner {
@@ -21,43 +19,37 @@ interface BluetoothScanner {
 }
 
 // Note: starting/stopping 5 or more times within a 30 second window will make the next startScan
-// call silently fail.
+// call silently fail. TODO: Can that be reconciled in the results() flow?
 class AndroidBluetoothScanner @Inject constructor(
     private val bluetoothAdapter: BluetoothAdapter,
     private val scanningConfig: ScanningConfig
 ) : BluetoothScanner {
 
-    override fun results(): Flow<BluetoothScanner.Result> {
-        val flow = callbackFlow<BluetoothScanner.Result> {
-            val bluetoothLeScanner = requireNotNull(bluetoothAdapter.bluetoothLeScanner) {
-                "Either BT wasn't turned on or relevant permissions weren't actively granted!"
-            }
-            val callback = object : ScanCallback() {
-                override fun onScanResult(callbackType: Int, result: ScanResult) {
-                    if (isClosedForSend) return
-                    val addresses = listOf(result.device.address)
-                    offer(BluetoothScanner.Result.Devices(addresses))
-                }
-
-                override fun onScanFailed(errorCode: Int) {
-                    if (isClosedForSend) return
-                    offer(BluetoothScanner.Result.Error(errorCode))
-                }
-
-                override fun onBatchScanResults(results: MutableList<ScanResult>) {
-                    if (isClosedForSend) return
-                    val addresses = results.map { result -> result.device.address }
-                    offer(BluetoothScanner.Result.Devices(addresses))
-                }
+    override fun results(): Flow<BluetoothScanner.Result> = callbackFlow {
+        val bluetoothLeScanner = requireNotNull<BluetoothLeScanner>(bluetoothAdapter.bluetoothLeScanner) {
+            "Either BT wasn't turned on or relevant permissions weren't actively granted!"
+        }
+        val callback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                if (isClosedForSend) return
+                val addresses = listOf(result.device.address)
+                offer(BluetoothScanner.Result.Devices(addresses))
             }
 
-            bluetoothLeScanner.startScan(scanningConfig.filters, scanningConfig.scanSettings, callback)
+            override fun onScanFailed(errorCode: Int) {
+                if (isClosedForSend) return
+                offer(BluetoothScanner.Result.Error(errorCode))
+            }
 
-            awaitClose { bluetoothLeScanner.stopScan(callback) }
+            override fun onBatchScanResults(results: MutableList<ScanResult>) {
+                if (isClosedForSend) return
+                val addresses = results.map { result -> result.device.address }
+                offer(BluetoothScanner.Result.Devices(addresses))
+            }
         }
 
-        return flow
-            .conflate()
-            .onEach { delay(scanningConfig.emissionDelay.toLongMilliseconds()) }
+        bluetoothLeScanner.startScan(scanningConfig.filters, scanningConfig.scanSettings, callback)
+
+        awaitClose { bluetoothLeScanner.stopScan(callback) }
     }
 }
--- app/src/main/kotlin/nick/template/data/ScanningConfig.kt
+++ app/src/main/kotlin/nick/template/data/ScanningConfig.kt
@@ -3,21 +3,17 @@ package nick.template.data
 import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanSettings
 import javax.inject.Inject
-import kotlin.time.Duration
-import kotlin.time.milliseconds
 
 interface ScanningConfig {
     val filters: List<ScanFilter>?
     val scanSettings: ScanSettings
-    val emissionDelay: Duration
 }
 
 class DefaultScanningConfig @Inject constructor() : ScanningConfig {
     override val filters: List<ScanFilter>? = null
 
     override val scanSettings: ScanSettings = ScanSettings.Builder()
-        .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+        .setReportDelay(2_500L)
         .build()
-
-    override val emissionDelay: Duration = 400.milliseconds
 }

39bc87e21628078194f79442f4f9aaa8b180120e
Nick Rose <nihk@github.com>
2021-05-08 23:54:01 -0400

    Add empty tests to be filled out

---
+++ app/src/androidTest/kotlin/nick/template/ui/devices/DevicesFragmentTest.kt
@@ -0,0 +1,60 @@
+package nick.template.ui.devices
+
+import org.junit.Test
+
+class DevicesFragmentTest {
+    @Test
+    fun permissionsWereRequested_whenNoneWereGranted() {
+
+    }
+
+    @Test
+    fun permissionsWereNotRequested_whenTheyWereAlreadyGranted() {
+
+    }
+
+    @Test
+    fun permissionsRationaleWasDisplayed_whenRequestingMoreThanOnce() {
+
+    }
+
+    @Test
+    fun bluetoothIsAskedToBeTurnedOn_whenItIsOff() {
+
+    }
+
+    @Test
+    fun bluetoothIsNotAskedToBeTurnedOn_whenItIsOn() {
+
+    }
+
+    @Test
+    fun bluetoothRationaleWasDisplayed_whenAskingMoreThanOnce() {
+
+    }
+
+    @Test
+    fun locationIsAskedToBeTurnedOn_whenItIsOff() {
+
+    }
+
+    @Test
+    fun locationIsNotAskedToBeTurnedOn_whenItIsOn() {
+
+    }
+
+    @Test
+    fun locationRationalWasDisplayed_whenAskingMoreThanOnce() {
+
+    }
+
+    @Test
+    fun devicesWereScanned_whenBluetoothWasUsable() {
+
+    }
+
+    @Test
+    fun devicesWereNotScanned_whenBluetoothWasNotUsable() {
+
+    }
+}

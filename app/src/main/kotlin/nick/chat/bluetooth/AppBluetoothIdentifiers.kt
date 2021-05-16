76a43d1eebd8e2d588dbd37586d9a726d3644241
Nick Rose <nihk@github.com>
2021-05-15 20:37:38 -0400

    Remove todo after testing

--- app/src/main/kotlin/nick/chat/bluetooth/AppBluetoothIdentifiers.kt
+++ app/src/main/kotlin/nick/chat/bluetooth/AppBluetoothIdentifiers.kt
@@ -14,7 +14,6 @@ class AppBluetoothIdentifiers @Inject constructor(
     override val service: UUID = UUID.fromString("0000b81d-0000-1000-8000-00805f9b34fb")
     override val message: UUID = UUID.fromString("7db3e235-3608-41f3-a03c-955fcbd2ea4b")
     override val serviceData: ByteArray = run {
-        // fixme: need to test this encoding/decoding works as i want it to
         sharedPreferences.getString(KEY_SERVICE_DATA, null)?.toByteArray(Charsets.ISO_8859_1)
             ?: Random.nextBytes(ByteArray(serviceDataConfig.byteSize)).also { bytes ->
                 sharedPreferences.edit()
